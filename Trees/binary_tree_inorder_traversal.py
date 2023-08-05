457d108064e8d832e76142a02c68b7d4b675da96
BusayoSamuel <busayosalisu@gmail.com>
2023-08-05 14:17:34 +0100

    Trees qs

---
+++ Trees/binary_tree_inorder_traversal.py
@@ -0,0 +1,34 @@
+"""
+https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+"""
+
+class RecursiveSolution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = [] #behaves as a global variable to the inorder function
+
+        def inorder(root):
+            if not root:
+                return  #pops back to the parent node
+        
+            inorder(root.left) #inorder the left sub-tree
+            res.append(root.val)
+            inorder(root.right) #inorder the right sub-tree
+
+        inorder(root)
+        return 
+    
+class IterativeSolution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        stack = []
+        curr = root
+
+        while curr or stack:
+            while curr: # not "curr.left"
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop() #allow the stack to be popped even if curr is null
+            res.append(curr.val)
+            curr = curr.right #this will always get called if curr.left is null
+
+        return res
