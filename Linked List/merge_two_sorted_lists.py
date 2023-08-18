49d66dcb15ecd25854917a53e981e9402718cd38
BusayoSamuel <busayosalisu@gmail.com>
2023-08-18 20:47:03 +0100

    linked list qs

---
+++ Linked List/merge_two_sorted_lists.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution: #O(n) time complexity, #O(1) space complexity
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        curr = dummy
+
+        while list1 and list2:
+            if list1.val > list2.val:
+                curr.next = list2
+                list2 = list2.next
+            else:
+                curr.next = list1
+                list1 = list1.next
+            curr = curr.next
+
+        if list1:
+            curr.next = list1
+        else:
+            curr.next = list2
+
+        return dummy.next
---
+++ Linked List/reverse_linked_list.py
@@ -0,0 +1,21 @@
+"""
+https://leetcode.com/problems/reverse-linked-list/description/
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution: #O(n) time complexity, #O(1) space complexity
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+
+        return prev
