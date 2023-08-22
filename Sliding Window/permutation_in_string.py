2df78d937417f8bf23699c2741828e8b13779765
BusayoSamuel <busayosalisu@gmail.com>
2023-08-22 11:31:59 +0100

    sliding window qs

---
+++ Sliding Window/permutation_in_string.py
@@ -0,0 +1,51 @@
+"""
+https://leetcode.com/problems/permutation-in-string/description/
+"""
+
+class Solution1:  #O(n) time complexity, #O(n) space complexity where n is len(s2)
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        hashmap = {} #value:count
+        l = 0
+
+        if len(s1) > len(s2):
+            return False
+
+        for i in range(len(s1)): #Initial comparison
+            hashmap[s2[i]] = hashmap.get(s2[i], 0) + 1
+            hashmap[s1[i]] = hashmap.get(s1[i], 0) - 1
+
+        if min(hashmap.values()) == max(hashmap.values()) == 0:  #Ensures that all characters in s1 are accounted for
+            return True
+
+        for r in range(len(s1), len(s2)): #Sliding Window
+            hashmap[s2[r]] = hashmap.get(s2[r], 0) + 1
+            hashmap[s2[l]] -= 1
+            l += 1
+
+            if min(hashmap.values()) == max(hashmap.values()) == 0:
+                return True
+
+        return False
+    
+
+class Solution2: #Alternative structure
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        hashmap = {} #value:count
+        l = 0
+
+        if len(s1) > len(s2):
+            return False
+
+        for i in range(len(s1)):
+            hashmap[s2[i]] = hashmap.get(s2[i], 0) + 1
+            hashmap[s1[i]] = hashmap.get(s1[i], 0) - 1
+
+        for r in range(len(s1), len(s2)):
+            if min(hashmap.values()) == max(hashmap.values()) == 0: # Make a check first
+                return True
+
+            hashmap[s2[r]] = hashmap.get(s2[r], 0) + 1
+            hashmap[s2[l]] -= 1
+            l += 1    
+
+        return min(hashmap.values()) == max(hashmap.values()) == 0
