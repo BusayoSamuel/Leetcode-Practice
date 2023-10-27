91d77d98bd6a7cd6c9b5825e29e34646817efd03
BusayoSamuel <busayosalisu@gmail.com>
2023-10-27 18:34:10 +0100

    linked list qs

---
+++ Linked List/swapping_nodes_in_a_linked_list.py
@@ -0,0 +1,46 @@
+"""
+https://leetcode.com/problems/swapping-nodes-in-a-linked-list/description/
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution: #Time complexity O(n), Space complexity O(1)
+    def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        left = head
+        right = head
+        end = head
+
+        for i in range(k):
+            end = end.next
+            if i < k-1:
+                left = left.next
+
+        while end:
+            end = end.next
+            right = right.next
+
+        left.val, right.val = right.val, left.val
+
+        return head
+    
+
+class Solution: #Perhaps a cleaner version, same complexity
+    def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        curr = head
+
+        for i in range(k-1):
+            curr = curr.next
+
+        left = curr
+        right = head
+
+        while curr.next:
+            curr = curr.next
+            right = right.next
+
+        left.val, right.val = right.val, left.val
+
+        return head
