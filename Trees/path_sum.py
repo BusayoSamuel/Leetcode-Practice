89ea166a252a42b2d32eb8e8c2a24ca1b4cf2808
BusayoSamuel <busayosalisu@gmail.com>
2023-11-25 09:49:15 +0000

    tree qs

---
+++ Trees/path_sum.py
@@ -0,0 +1,24 @@
+"""
+https://leetcode.com/problems/path-sum/description/
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution: #Time complexity O(n), Space complexity O(n)
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        
+        def dfs(root, curSum):
+            if not root:
+                return False
+
+            curSum += root.val
+            if not root.left and not root.right:
+                return curSum == targetSum
+
+            return dfs(root.left, curSum) or dfs(root.right, curSum)
+
+        return dfs(root,0)
