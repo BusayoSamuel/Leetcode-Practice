e70bbbf50b93ea473898a062321556ab660e5614
BusayoSamuel <busayosalisu@gmail.com>
2024-01-04 20:34:38 +0000

    binary search qs

---
+++ Binary Search/time_based_key_value_store.py
@@ -0,0 +1,68 @@
+"""
+https://leetcode.com/problems/time-based-key-value-store/description/
+"""
+
+class TimeMap:
+
+    def __init__(self):
+        self.hashmap = {}
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None: #Time complexity O(n)
+        self.hashmap[key] = self.hashmap.get(key, []) + [[value, timestamp]]
+
+    def get(self, key: str, timestamp: int) -> str: #Time complexity O(logn)
+        if key not in self.hashmap:
+            return ""
+
+        l = 0
+        r = len(self.hashmap[key]) - 1
+        res = -1
+
+        while l <= r:
+            m = (r + l)//2
+
+            if self.hashmap[key][m][1] > timestamp:
+                r = m - 1
+            elif self.hashmap[key][m][1] < timestamp:
+                l = m + 1
+                res = max(res, m)
+            else:
+                return self.hashmap[key][m][0]
+
+        return "" if res == -1 else self.hashmap[key][res][0]
+
+
+#Alternative solution
+class TimeMap:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.keyStore = {}  # key : list of [val, timestamp]
+
+    def set(self, key: str, value: str, timestamp: int) -> None: #Time complexity O(1)
+        if key not in self.keyStore:
+            self.keyStore[key] = []
+        self.keyStore[key].append([value, timestamp])
+
+    def get(self, key: str, timestamp: int) -> str: #Time complexity O(logn)
+        res, values = "", self.keyStore.get(key, [])
+        l, r = 0, len(values) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if values[m][1] <= timestamp:
+                res = values[m][0]
+                l = m + 1
+            else:
+                r = m - 1
+        return res
+
+
+        
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
