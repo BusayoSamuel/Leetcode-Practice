f9b4cbecdb182967d2c17a897709f81132223789
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2024-02-06 14:19:39 +0000

    sliding window qs

--- Sliding Window/find_k_closest_elements.py
+++ Sliding Window/find_k_closest_elements.py
@@ -1,15 +1,19 @@
-# Log(n) + k
-# More code but also more intuitive
-class Solution:
-    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        l, r = 0, len(arr) - 1
+"""
+https://leetcode.com/problems/find-k-closest-elements/description/
+"""
 
-        # Find index of x or the closest val to x
+class IntuitiveSolution: #Time complexity O(logn + k), Space complexity O(1)
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        l = 0
+        r = len(arr) - 1
+
+	#Find search the closest value to target
         val, idx = arr[0], 0
         while l <= r:
-            m = (l + r) // 2
-            curDiff, resDiff = abs(arr[m] - x), abs(val - x)
-            if curDiff < resDiff or (curDiff == resDiff and arr[m] < val):
+            m = (r+l)//2
+
+            curDiff, resDiff = abs(arr[m] - x), abs(arr[idx] - x)
+            if curDiff < resDiff or (curDiff == resDiff and arr[m] < arr[idx]): #keeps track of closest value to target
                 val, idx = arr[m], m
 
             if arr[m] < x:
@@ -19,27 +23,38 @@ class Solution:
             else:
                 break
 
-        l = r = idx
-        for i in range(k - 1):
-            if l == 0:
-                r += 1
-            elif r == len(arr) - 1 or x - arr[l - 1] <= arr[r + 1] - x:
-                l -= 1
+        start = idx 
+        end = idx
+
+        while end - start + 1 < k: #Starting from the closest value, we enlarge the window as appropriate
+            if start - 1 >= 0 and end + 1 < len(arr):
+                if abs(arr[start-1] - x) < abs(arr[end+1] - x):
+                    start -= 1
+                elif abs(arr[start-1] - x) > abs(arr[end+1] - x):
+                    end += 1
+                else:
+                    if arr[start-1] <= arr[end+1]:
+                        start -= 1
+                    else:
+                        end += 1
+            elif start - 1 >= 0 :
+                start -= 1
             else:
-                r += 1
-        return arr[l : r + 1]
+                end += 1
+
+        return arr[start: end + 1] 
 
 
-# Log(n-k) + k
-# Elegant but very difficult to understand
-class Solution:
+class SlightlyBetterSolution: #Time complexity O(log(n-k)), Space complexity O(1)
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
         l, r = 0, len(arr) - k
 
         while l < r:
-            m = (l + r) // 2
-            if x - arr[m] > arr[m + k] - x:
-                l = m + 1
+            m = (r + l)//2
+            if x - arr[m] > arr[m+k] - x: #access if the lefmost value in curr window is less close to value immediately right of window 
+                l = m + 1 #therefore the values to left would never be closer
             else:
-                r = m
-        return arr[l : l + k]
+                r = m therefore values to right wouldn't be closer
+
+        return arr[l:l+k]
+        
