280184934585ef55bb0ba66a27977182e8349436
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2024-02-07 07:29:40 +0000

    Update online_stock_span.py

--- Stacks/online_stock_span.py
+++ Stacks/online_stock_span.py
@@ -10,8 +10,8 @@ class StockSpanner:
 
     def next(self, price: int) -> int: #Time complexity O(n), Space complexity O(n)
         count = 1
-        while self.stack and self.stack[-1][0] <= price:
-            count += self.stack.pop()[1] # the span of recorded so no need to keep it stored in the stack 
+        while self.stack and self.stack[-1][0] <= price: #In the worst case only one of the prices would need to loop through the length of the stack so time complexity is O(n + n) => O(n)
+            count += self.stack.pop()[1] # the span is recorded so no need to keep it stored in the stack 
 
         self.stack.append([price, count])
         return count
