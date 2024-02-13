b0651bfc61f98e6028d05415206cc22be62e3a04
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2024-02-13 16:06:20 +0000

    Update minimum_operations_to_reduce_x_to_zero.py

--- Sliding Window/minimum_operations_to_reduce_x_to_zero.py
+++ Sliding Window/minimum_operations_to_reduce_x_to_zero.py
@@ -20,3 +20,32 @@ class Solution: #Time complexity O(n), Space complexity O(1)
                 maxW = max(maxW, r-l+1)
 
         return -1 if maxW == -1 else len(nums) - maxW
+
+
+class InefficientSolution: #Time complexiy O(n^2), Space complexity O(n)
+    def minOperations(self, nums: List[int], x: int) -> int:
+        prefix = [] #sum of nums from the left
+        postfix = [] #sum of nums from the right
+        curSum = 0
+        res = len(nums) + 1
+
+        for i in range(len(nums)):
+            curSum += nums[i]
+            prefix.append(curSum)
+
+        curSum = 0
+
+        for j in range(len(nums)-1, -1, -1): 
+            curSum += nums[j]
+            postfix = [curSum] + postfix
+
+        for i in range(len(nums)):
+            if prefix[i] == x: #checking if the the prefix on its own would meet the target
+                res = min(res, i + 1)
+            for j in range(i, len(nums)):
+                if prefix[i] + postfix[j] == x:
+                    res = min(res, i + (-j + len(nums)) + 1)
+                if postfix[j] == x:  #checking if the the postfix on its own would meet the target
+                    res = min(res, -j + len(nums))
+
+        return res if res != len(nums) + 1 else -1
