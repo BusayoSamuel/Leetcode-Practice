c52d1b674da33c8415e870656d345ca0be9a49ee
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2024-02-23 16:52:27 +0000

    linked list qs

---
+++ Linked List/reverse_linked_list_ii.py
@@ -0,0 +1,76 @@
+"""
+https://leetcode.com/problems/reverse-linked-list-ii/description/
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class NotTheCleanestSolution: #Time complexity O(n), Space complexity O(1)
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        res = head
+        
+
+        node = dummy
+        count = 0
+
+        while count != left - 1:
+            node = node.next
+            count += 1
+
+        curr = node.next
+        start = node
+
+        while count != right - 1:
+            node = node.next
+            count += 1
+
+        end = node.next.next
+
+        prev = None
+        tail = curr
+        count = left
+
+        while count != right + 1:
+            tmp = curr
+            curr = curr.next
+            tmp.next = prev
+            prev = tmp
+            count += 1
+
+        start.next = prev
+        tail.next = end
+
+        return dummy.next
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution: #Cleaner solution, same complexity
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+
+        leftPrev = dummy
+        cur = dummy.next
+
+        for i in range(left-1):
+            leftPrev = leftPrev.next
+            cur = cur.next
+
+        prev = None
+        tail = cur
+
+        for i in range(right - left + 1):
+            tmpNext = cur.next
+            cur.next = prev
+            prev = cur
+            cur = tmpNext
+
+        leftPrev.next = prev
+        tail.next = cur
+
+        return dummy.next
