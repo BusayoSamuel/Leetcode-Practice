80d5fb6ed744b93b03504ea0e54534aa8fb1701c
BusayoSamuel <busayosalisu@gmail.com>
2024-04-05 09:54:42 +0100

    heap qs

---
+++ Heap/k_closest_points_to_origin.py
@@ -0,0 +1,34 @@
+"""
+https://leetcode.com/problems/k-closest-points-to-origin/description/
+"""
+class MySolution: #Time complexity O(nlogn), Space complexity O(n) due to sorting
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        def dist(point):
+            return math.sqrt(((-point[0])**2)+((-point[1])**2))
+
+        points.sort(key=lambda x: dist(x))
+
+
+        return points[:k]
+    
+
+class OptimalSolution: #Time complexity O(klogn) as we only call heappop k times, Space complexity O(n)
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+
+        def dist(point):
+            return ((-point[0])**2)+((-point[1])**2) #no need to find square root, if sqrt(x) > sqrt(y), then x > y
+
+        minHeap = list(map(lambda x: [dist(x), x], points)) #need to always convert a map to list
+
+        heapq.heapify(minHeap) 
+
+        res = []
+
+        while k:
+            dist, point = heapq.heappop(minHeap)
+            res.append(point)
+            k -= 1
+
+        return res
+
+        
