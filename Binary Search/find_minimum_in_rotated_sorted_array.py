5546dab6532a128d991cf1ed2aa149b454e5d9d0
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2024-04-09 17:45:50 +0100

    Update find_minimum_in_rotated_sorted_array.py

--- Binary Search/find_minimum_in_rotated_sorted_array.py
+++ Binary Search/find_minimum_in_rotated_sorted_array.py
@@ -2,7 +2,7 @@
 https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 """
 
-class Solution:
+class Solution: #Time complexity O(logn), Space complexity O(1)
     def findMin(self, nums: List[int]) -> int:
         l = 0
         r = len(nums) - 1
@@ -21,3 +21,21 @@ class Solution:
                 l = m + 1
 
         return res
+
+
+class MySolution: #Same complexity
+    def findMin(self, nums: List[int]) -> int:
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            m = (r+l)//2
+
+
+            if nums[m] > nums[-1]: # this means the first half sorted portion is to the right
+                l = m + 1 
+            else: # now we're in the first half sorted portion
+                if m > 0 and m < len(nums) - 1 and nums[m-1] < nums[m] < nums[m+1]: #if m == 0 or len(nums)-1 or the left neighbour > right neighbour then it has to be the lowest number 
+                    r = m - 1
+                else:
+                    return nums[m]
