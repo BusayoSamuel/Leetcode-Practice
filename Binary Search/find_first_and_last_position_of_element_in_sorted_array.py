d9685ec30f02602d634b2bd8e2a185528f015084
BusayoSamuel <busayosalisu@gmail.com>
2024-05-21 07:16:01 +0100

    binary search qs update

--- Binary Search/find_first_and_last_position_of_element_in_sorted_array.py
+++ Binary Search/find_first_and_last_position_of_element_in_sorted_array.py
@@ -29,3 +29,32 @@ class Solution: #Time complexity O(logn), Space complexity O(1)
         search(False)
 
         return res
+    
+
+    class MySolution: #Time complexity O(n), Space complexity O(1)
+        def searchRange(self, nums: List[int], target: int) -> List[int]:
+            l = 0
+            r = len(nums) - 1
+            res = [-1, -1]
+
+            while l <= r:
+                m = (l+r)//2
+
+                if nums[m] > target:
+                    r = m - 1
+                elif nums[m] < target:
+                    l = m + 1
+                else:
+                    res[0] = m
+                    res[1] = m
+
+                    while res[0] - 1 >= 0 and nums[res[0]] == nums[res[0]-1]:
+                        res[0] -= 1
+
+                    while res[1] + 1 < len(nums) and nums[res[1]] == nums[res[1]+1]:
+                        res[1] += 1
+
+                    break
+                    
+            return res
+                
