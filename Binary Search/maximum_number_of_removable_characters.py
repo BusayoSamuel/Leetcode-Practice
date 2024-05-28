81252a8b8e36e208f3c04651c642afab30666429
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2024-05-28 08:42:10 +0100

    Update maximum_number_of_removable_characters.py

--- Binary Search/maximum_number_of_removable_characters.py
+++ Binary Search/maximum_number_of_removable_characters.py
@@ -1,7 +1,7 @@
 """
 https://leetcode.com/problems/maximum-number-of-removable-characters/description/
 """
-class Solution:
+class Solution: #Time complexity O((n+k)logm), #Space complexity(k)  where n is len(s) and m is len(removable)
     def maximumRemovals(self, s: str, p: str, removable: List[int]) -> int:
         k = 0
 
@@ -19,7 +19,7 @@ class Solution:
         while l<=r:
             m = (l+r)//2
 
-            removed = set(removable[:m+1])
+            removed = set(removable[:m+1]) #this will take O(k) time in the worst case
 
             if isSubseq():
                 k = max(k, m + 1)
@@ -28,3 +28,37 @@ class Solution:
                 r = m - 1 
 
         return k 
+
+
+class MySolution: #Same complexity
+    def maximumRemovals(self, s: str, p: str, removable: List[int]) -> int:
+        def check(remidx):
+            j = 0
+            for i in range(len(s)):
+                if i in remidx:
+                    continue
+
+                if p[j] == s[i]:
+                    j += 1
+                    if j >= len(p):
+                        return True
+
+            return False
+
+        l = 0
+        r = len(removable) - 1
+        res = 0
+
+        while l <= r:
+            m = (r+l)//2
+
+            if check(set(removable[:m+1])):
+                res = m + 1
+                l = m + 1
+            else:
+                r = m - 1
+
+        return res
+
+            
+        
