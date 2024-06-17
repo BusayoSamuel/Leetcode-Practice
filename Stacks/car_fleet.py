6e9f54b475d0a3111b0991376504ee439338c909
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2024-06-17 12:59:38 +0100

    Update car_fleet.py

--- Stacks/car_fleet.py
+++ Stacks/car_fleet.py
@@ -15,4 +15,22 @@ class Solution: #Time complexity O(nlogn), Space complexity O(n)
                 stack.append(time)
 
         return len(stack)
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        stack = []
+
+        cars = sorted(list(zip(position, speed)), reverse=True)
+
+        for car in cars:
+            pos, spd = car
+
+            time = (target - pos)/spd
+
+            if stack and time <= stack[-1]:
+                continue
+
+            stack.append(time)
+
+        return len(stack)
         
