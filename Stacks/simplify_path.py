e7d1fe546cb6ce68c6bc7cd9a6f1e45d0b1a2b92
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2024-06-24 06:38:08 +0100

    Update simplify_path.py

--- Stacks/simplify_path.py
+++ Stacks/simplify_path.py
@@ -42,6 +42,27 @@ class AlternativeSolution: #Different Structure, Same complexity
             r += 1 
 
         return "/" + "/".join(stack) 
+
+
+class ShortherSolution: #Time complexity O(n), Space complexity O(n)
+    def simplifyPath(self, path: str) -> str:
+
+        stack = []
+
+        for i in path.split("/"):
+            #  if i == "/" or i == '//', it becomes '' (empty string)
+
+            if i == "..":
+                if stack:
+                    stack.pop()
+            elif i == "." or i == '':
+                # skip "." or an empty string
+                continue
+            else:
+                stack.append(i)
+
+        res = "/" + "/".join(stack)
+        return res
             
 
         
