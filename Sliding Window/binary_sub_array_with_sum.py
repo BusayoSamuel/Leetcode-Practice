a40777523d48f615e666adc90865df4862708589
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2024-06-30 09:34:09 +0100

    sliding window qs

---
+++ Sliding Window/binary_sub_array_with_sum.py
@@ -0,0 +1,25 @@
+"""
+https://leetcode.com/problems/binary-subarrays-with-sum/description/
+"""
+
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        def slide(x):
+            if x < 0:
+                return 0
+
+            l = 0
+            total = 0
+            res = 0
+            for r in range(len(nums)):
+                total += nums[r]
+
+                while l <= r and total > x:
+                    total -= nums[l]
+                    l += 1
+
+                res += r-l+1  #we're adding all arrays that end with array nums[r] between nums[r] and nums[l] on each pass
+
+            return res
+
+        return slide(goal) - slide(goal-1) #Say is goal is 2, to get the subarrays we use sliding window for arrays with sum <= 2 and subtract arrays with sum <= 1 from it
