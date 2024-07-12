2d6e0ed8b644012b727e555a7233ad15716eafcc
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2024-07-12 08:00:37 +0100

    Update letter_combinations_of_a_phone_number.py

--- Backtracking/letter_combinations_of_a_phone_number.py
+++ Backtracking/letter_combinations_of_a_phone_number.py
@@ -33,4 +33,32 @@ class MySolution: #Time complexity O(4^n), Space complexity O(4^n)
         backtrack(0)
             
         return res
+
+
+class OtherSolution: #Same complexity as above
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "qprs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        def backtrack(i, curStr):
+            if len(curStr) == len(digits):
+                res.append(curStr)
+                return
+            for c in digitToChar[digits[i]]:
+                backtrack(i + 1, curStr + c)
+
+        if digits:
+            backtrack(0, "")
+
+        return res
+
         
