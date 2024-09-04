bb956e1ffa670a2da5f26ca939cf2731eeb18639
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2024-09-04 18:47:06 +0100

    tree qs

---
+++ Trees/all_possible_full_binary_trees.py
@@ -0,0 +1,30 @@
+"""
+https://leetcode.com/problems/all-possible-full-binary-trees/description/
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution: #Time complexity, Space complexity O(2^n)
+    def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
+        dp = { 0 : [], 1 : [ TreeNode() ] }
+
+        def backtrack(n):
+            if n in dp:
+                return dp[n]
+            
+            res = []
+            for l in range(n):
+                r = n - 1 - l
+                leftTrees, rightTrees = backtrack(l), backtrack(r)
+
+                for t1 in leftTrees:
+                    for t2 in rightTrees:
+                        res.append(TreeNode(0, t1, t2))
+            dp[n] = res
+            return res
+        
+        return backtrack(n)
