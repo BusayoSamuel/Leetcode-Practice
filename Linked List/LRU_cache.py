4ede423d49a5411cf14308c44821b34ce31b2693
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2024-10-01 08:51:48 +0100

    Update LRU_cache.py

--- Linked List/LRU_cache.py
+++ Linked List/LRU_cache.py
@@ -47,12 +47,66 @@ class LRUCache: #Optimal solution, Time complexity O(1), Space complexity O(capa
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
+
+#My Solution
+class Node:
+    def __init__(self, key = None, val = None, prev = None, next = None):
+        self.val = val
+        self.key = key
+        self.prev = prev
+        self.next = next
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.head = Node()
+        self.tail = Node()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.hashmap = {}
         
 
-        
+    def get(self, key: int) -> int:
+        if key in self.hashmap:
+            node = self.hashmap[key]
+            res = node.val
+            prevNode = node.prev
+            nextNode = node.next
+            temp = node
+            prevNode.next = nextNode
+            nextNode.prev = prevNode
+            prevNode = self.tail.prev
+            prevNode.next = temp
+            temp.prev = prevNode
+            temp.next = self.tail
+            self.tail.prev = temp
+            return res
+        else:
+            return -1      
 
+    def put(self, key: int, value: int) -> None:
+        if key in self.hashmap:
+            node = self.hashmap[key]
+            node.val = value
+            prevNode = node.prev
+            nextNode = node.next
+            prevNode.next = nextNode
+            nextNode.prev = prevNode
+            node.prev = self.tail.prev
+            node.next = self.tail
+            self.tail.prev.next = node
+            self.tail.prev = node
+        else:
+            node = Node(key, value, self.tail.prev, self.tail)
+            self.tail.prev.next = node
+            self.tail.prev = node
+            self.hashmap[key] = node
 
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+            if not self.cap:
+                delkey = self.head.next.key
+                self.head.next = self.head.next.next
+                self.head.next.prev = self.head
+                del self.hashmap[delkey]
+            else:
+                self.cap -= 1
