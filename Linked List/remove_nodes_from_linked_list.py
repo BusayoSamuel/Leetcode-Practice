256cc9a930cf4746b95e969b38a59e0aa52917db
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2024-10-01 08:56:18 +0100

    linked list qs

---
+++ Linked List/remove_nodes_from_linked_list.py
@@ -0,0 +1,40 @@
+"""
+https://leetcode.com/problems/remove-nodes-from-linked-list/description/
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class MySolution: #Time complexity O(n), Space complexity O(1)
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def reverse(node):
+            dummmy = ListNode(0, node)
+            prev = None
+            cur = node
+
+            while cur:
+                temp = cur
+                cur = cur.next
+                temp.next = prev
+                prev = temp
+
+            return prev
+
+
+        cur = reverse(head)
+        tail = cur
+
+        while cur:
+            while cur.next and cur.next.val < cur.val:
+                cur.next = cur.next.next
+
+            if not cur.next:
+                break
+
+            cur = cur.next
+
+        res = reverse(tail)
+
+        return res
