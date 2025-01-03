b83c037c4b572ef1ab52d1a247df9aa995a93b24
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-01-03 09:03:13 +0100

    two pointer qs

---
+++ Two Pointers/bag_of_tokens.py
@@ -0,0 +1,54 @@
+"""
+https://leetcode.com/problems/bag-of-tokens/submissions/1494864761/
+"""
+
+class MySolution: #Time complexity O(n), Space complexity O(n) due to sorting
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        tokens.sort()
+        l = 0
+        r = len(tokens)-1
+        res = 0
+        score = 0
+
+        if not tokens or power < tokens[l]:
+            return 0
+
+        while l <= r:
+            while score > 0 and l <= r and power < tokens[r]:
+                power += tokens[r]
+                score -= 1
+                r -= 1
+
+            while l <= r and power >= tokens[l]:
+                power -= tokens[l]
+                score += 1
+                l += 1
+
+            res = max(res, score)
+
+        return res
+
+class CleanerSolution: #Same complexity as above
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        l = 0
+        r = len(tokens)-1
+        res = 0
+        score = 0
+        tokens.sort()
+
+        while l <= r:
+            if power >= tokens[l]:
+                power -= tokens[l]
+                score += 1
+                l += 1
+                res = max(res, score)
+            elif score > 0:
+                power += tokens[r]
+                score -= 1
+                r -= 1
+            else:
+                break
+
+        return res
+
+            
