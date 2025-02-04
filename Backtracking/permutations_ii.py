45dc87b96e4e300fb1f17054e6ca76dd4142b369
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-02-04 08:55:21 +0000

    Update permutations_ii.py

--- Backtracking/permutations_ii.py
+++ Backtracking/permutations_ii.py
@@ -2,7 +2,7 @@
 https://leetcode.com/problems/permutations-ii/description/
 """
 
-class MySolution: #Time complexity O(n^n!), Space complexity O(n^n!)
+class MySolution: #Time complexity O(nlogn), Space complexity O(n)
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         res = []
         curSet = []
@@ -23,4 +23,32 @@ class MySolution: #Time complexity O(n^n!), Space complexity O(n^n!)
 
         backtrack(nums)
         return res
+
+class MyOtherSolution: #Time complexity O(nlogn) due to sorting and nlogn > n!, Space complexity O(n)
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        cur = []
+        res = []
+        picked = set()
+        nums.sort()
+
+        def dfs():
+            if len(cur) == len(nums):
+                res.append(cur.copy())
+                return
+
+            for j in range(len(nums)):
+                if j in picked or (j > 0 and nums[j] == nums[j-1] and j - 1 not in picked):
+                    continue
+
+                cur.append(nums[j])
+                picked.add(j)
+                dfs()
+                cur.pop()
+                picked.remove(j)
+
+
+        dfs()
+        return res
+        
+
         
