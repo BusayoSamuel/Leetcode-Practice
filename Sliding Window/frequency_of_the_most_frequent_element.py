023d87d439383ad19fede97093c5948e457a8963
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-02-09 08:39:39 +0000

    Update frequency_of_the_most_frequent_element.py

--- Sliding Window/frequency_of_the_most_frequent_element.py
+++ Sliding Window/frequency_of_the_most_frequent_element.py
@@ -62,3 +62,31 @@ class Attempt2: #O(n^2) time complexity #O(n) space complexity, Not efficient en
             res = max(res, count)
                 
         return res
+
+
+class MySolution: #Time complexity O(n), Space complexity O(n)
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        res = 0
+        l = 0
+        total = 0
+        maxNum = -1
+
+        for r in range(len(nums)):
+            total += nums[r]
+            maxNum = max(maxNum, nums[r])
+
+            window = r - l + 1
+            while r > l and (maxNum * window - total) > k:
+                total -= nums[l]
+                window -= 1
+                l += 1
+
+            res = max(res, r-l+1)
+            
+
+        return res
+
+
+
+        
