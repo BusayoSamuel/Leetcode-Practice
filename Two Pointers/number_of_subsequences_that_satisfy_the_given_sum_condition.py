e33ecf8cb8f92f8364f295a35e4f515c4a4f9a9c
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-03-06 09:21:21 +0000

    Update number_of_subsequences_that_satisfy_the_given_sum_condition.py

--- Two Pointers/number_of_subsequences_that_satisfy_the_given_sum_condition.py
+++ Two Pointers/number_of_subsequences_that_satisfy_the_given_sum_condition.py
@@ -16,4 +16,26 @@ class Solution: #Time complexity O(nlogn), Space complexity O(n) due to sorting
                 res += 2**(r-l) #This works out all the combinations you can have for this minimum value
 
         return res % (10**9 + 7)
+
+
+class MySolution: #Same complexity as above
+    def numSubseq(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        res = 0
+
+        r = len(nums) - 1 #really key to start r from the end to avoid the repeated work, we're considering the next minimum in each loop so need to consider higher values of max each time
+        for l in range(len(nums)):
+
+            while l <= r and nums[l] + nums[r] > target:
+                r -= 1
+
+            if r < l: #this suggest to no other minimum would work so we can close here
+                break
+
+            res += 2**(r - l) 
+
+
+        return res % (10**9 + 7)
+
+
         
