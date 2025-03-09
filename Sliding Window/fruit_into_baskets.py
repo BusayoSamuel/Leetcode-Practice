f4664148eac11a5b650790fac56b485517d6edb0
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-03-09 22:21:09 +0000

    Update fruit_into_baskets.py

--- Sliding Window/fruit_into_baskets.py
+++ Sliding Window/fruit_into_baskets.py
@@ -21,6 +21,25 @@ class Solution: #Time complexity O(n), Space complexity O(1)
 
         return res
 
+
+class MySolution: #Same complexity as above
+    def totalFruit(self, fruits: List[int]) -> int:
+        l = 0
+        res = 0
+        hashmap = defaultdict(int)
+
+        for r in range(len(fruits)):
+            hashmap[fruits[r]] += 1
+            while len(hashmap) > 2:
+                hashmap[fruits[l]] -= 1
+                if hashmap[fruits[l]] == 0:
+                    hashmap.pop(fruits[l])
+                l += 1
+
+            res = max(res, r - l + 1)
+
+        return res
+
             
 
         
