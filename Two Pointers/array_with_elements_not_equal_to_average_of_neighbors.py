b2e740b61ada395738df13503c0d15c83afe49ab
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-03-13 08:20:13 +0000

    Update array_with_elements_not_equal_to_average_of_neighbors.py

--- Two Pointers/array_with_elements_not_equal_to_average_of_neighbors.py
+++ Two Pointers/array_with_elements_not_equal_to_average_of_neighbors.py
@@ -34,3 +34,52 @@ class Solution2: #Time complexity O(nlogn), Space complexity O(n) due to sorting
                 r -= 1
         
         return res
+
+class MyOtherSolution: #Same complexity as Solution1, Inefficient
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        def isValid(n):
+            l = 0
+            i = 1
+            r = 2
+
+            while r < len(n):
+                ave = (n[l] + n[r])/2
+                if n[i] == ave:
+                    return False
+                
+                i += 1
+                l += 1
+                r += 1
+
+            return True
+
+        cur = []
+        visit = set()
+
+        def dfs():
+            if len(cur) >= len(nums):
+                if isValid(cur):
+                    return [True, cur]
+
+                return [False, None]
+
+
+            res = None
+            for i in range(len(nums)):
+                if nums[i] in visit:
+                    continue
+
+                cur.append(nums[i])
+                visit.add(nums[i])
+                valid, res = dfs()
+                if valid:
+                    return [valid, res]
+                visit.remove(nums[i])
+                cur.pop()
+                
+
+            return [False, None]
+
+
+        return dfs()[1]
+
