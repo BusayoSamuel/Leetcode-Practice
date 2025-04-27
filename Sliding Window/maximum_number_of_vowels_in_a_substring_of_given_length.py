0ed0219f0ed6db13ec1cbe289791839393cf0788
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-04-27 08:50:29 +0100

    Update maximum_number_of_vowels_in_a_substring_of_given_length.py

--- Sliding Window/maximum_number_of_vowels_in_a_substring_of_given_length.py
+++ Sliding Window/maximum_number_of_vowels_in_a_substring_of_given_length.py
@@ -22,4 +22,26 @@ class Solution: #Time complexity O(n), Space Complexity O(1)
         return res
 
 
+class MySolution: #Same complexity as above
+    def maxVowels(self, s: str, k: int) -> int:
+        vowels = set(['a', 'e', 'i', 'o', 'u'])
+        res = 0
+        l = 0
+        cur = 0
+
+        for r in range(len(s)):
+            while r - l + 1 > k:
+                if s[l] in vowels:
+                    cur -= 1
+                l += 1
+                
+                
+            if s[r] in vowels:
+                cur += 1
+
+            res = max(cur, res)
+
+        return res
+
+
         
