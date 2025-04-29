0f5e831283d6850211269ecad7b2b4c9c1d4c82a
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-04-29 10:39:10 +0100

    Update matchsticks_to_square.py

--- Backtracking/matchsticks_to_square.py
+++ Backtracking/matchsticks_to_square.py
@@ -59,20 +59,20 @@ class MySolution: #Time complexity O(4^n), Space complexity O(n)
 
 class CleanerSolution: #Same complexity
     def makesquare(self, matchsticks: List[int]) -> bool:
-        if sum(matchsticks) % 4 != 0:
+        if sum(matchsticks) % 4 != 0: #check if it's even possible to 4 integer sides
             return False
             
         target = sum(matchsticks)//4
         sides = [0] * 4
 
-        matchsticks.sort(reverse=True)
+        matchsticks.sort(reverse=True) #makes it quciker to determine if it's possible to make a square if the largest matchstick is bigger than the target side length
 
         def backtrack(i):
 
             if i >= len(matchsticks):
                     return True
 
-            for j in range(4): 
+            for j in range(4): #try placing a matchstick in every side 
                 if sides[j] + matchsticks[i] <= target:
                     sides[j] += matchsticks[i]
                     if backtrack(i+1):
