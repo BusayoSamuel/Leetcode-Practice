e2575a1be1b6739f4787f71e37a7c56265f5de8d
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-05-04 15:22:45 +0100

    Update minimum_number_of_flips_to_make_the_binary_string_alternating.py

--- Sliding Window/minimum_number_of_flips_to_make_the_binary_string_alternating.py
+++ Sliding Window/minimum_number_of_flips_to_make_the_binary_string_alternating.py
@@ -43,6 +43,54 @@ class Solution: #Time complexity O(n), Space complexity O(n)
         return res
 
 
+class MySolution: #Same complexity as above
+    def minFlips(self, s: str) -> int:
+        n = len(s)
+        zero = '0'
+        one = '1'
+        alt1 = ''
+        alt2 = ''
+        s += s
+        res = math.inf
+        
+
+        for i in range(len(s)):
+            alt1 += one if i % 2 else zero
+            alt2 += zero if i % 2 else one
+
+        count1 = 0
+        count2 = 0
+        l = 0
+        r = 0
+        while r < len(s):
+            if s[r] != alt1[r]:
+                count1 += 1
+            if s[r] != alt2[r]:
+                count2 += 1
+
+
+            if r-l+1 == n:
+                res = min(res, count1, count2)
+
+                if s[l] != alt1[l]:
+                    count1 -= 1
+
+                if s[l] != alt2[l]:
+                    count2 -= 1
+
+                l += 1
+                
+
+            
+            r += 1
+
+
+        return res
+
+
+        
+
+
         
         
         
