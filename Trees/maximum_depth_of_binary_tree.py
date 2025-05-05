03ac0a21ad68ef62efbe65e8d754b7ae7a042c5f
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-05-05 11:28:54 +0100

    Update maximum_depth_of_binary_tree.py

--- Trees/maximum_depth_of_binary_tree.py
+++ Trees/maximum_depth_of_binary_tree.py
@@ -15,3 +15,11 @@ class Solution: #Time Complexity O(n), Space Complexity O(n)
             return 0
         
         return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+class MySolution: #Same complexity as above
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+
+        if not root:
+            return 0
+
+        return max(1 + self.maxDepth(root.right), 1 + self.maxDepth(root.left))
