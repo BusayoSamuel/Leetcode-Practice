d7f1fd791e889e6e8ac0a0b1595a6e8f2774f03e
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-05-24 16:22:33 +0100

    1-dp qs

---
+++ 1-D Dynamic Programming/longest_increasing_sequence.py
@@ -0,0 +1,14 @@
+"""
+https://leetcode.com/problems/longest-increasing-subsequence/description/
+"""
+
+class Solution: #Time complexity O(n^1), Space complexity O(n)
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        LIS = [1] * len(nums)
+
+        for i in range(len(nums)-1,-1,-1):
+            for j in range(i+1, len(nums)):
+                if nums[i] < nums[j]:
+                    LIS[i] = max(LIS[i], 1 + LIS[j])
+
+        return max(LIS)
