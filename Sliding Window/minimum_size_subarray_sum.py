5809a9750c8d431bac9bdbb5084044b238adec8f
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-06-08 08:05:18 +0100

    Update minimum_size_subarray_sum.py

--- Sliding Window/minimum_size_subarray_sum.py
+++ Sliding Window/minimum_size_subarray_sum.py
@@ -23,4 +23,21 @@ class Solution: #Time complexity O(n), Space complexity O(1)
 
         return 0 if res == float("Inf") else res
 
+
+class MySolution: #Same complexity as above
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        l = 0
+        res = len(nums) + 1
+        cur = 0
+
+        for r in range(len(nums)):
+            cur += nums[r]
+
+            while cur >= target:
+                res = min(res, r - l + 1)
+                cur -= nums[l]
+                l += 1               
+
+        return res if res <= len(nums) else 0
+
         
