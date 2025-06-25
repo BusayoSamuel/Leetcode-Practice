5c5898b3d2d8a2c73a9d3dc77172f63d6ed8f28d
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-06-25 08:46:39 +0100

    graph qs

---
+++ Graphs/course_schedule.py
@@ -0,0 +1,33 @@
+"""
+https://leetcode.com/problems/course-schedule/description/
+"""
+
+class Solution: #Time complexity O(V+E), Space complexity O(V+E)
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # Map each course to its prerequisites
+        preMap = {i: [] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+
+        # Store all courses along the current DFS path
+        visiting = set()
+
+        def dfs(crs):
+            if crs in visiting:
+                # Cycle detected
+                return False
+            if preMap[crs] == []:
+                return True
+
+            visiting.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            visiting.remove(crs)
+            preMap[crs] = []
+            return True
+
+        for c in range(numCourses):
+            if not dfs(c):
+                return False
+        return True
