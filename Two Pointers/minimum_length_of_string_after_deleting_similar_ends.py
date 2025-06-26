4984b5364f704f2bbd0a0a794ac8b1489d5f3516
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-06-26 08:04:55 +0100

    Update minimum_length_of_string_after_deleting_similar_ends.py

--- Two Pointers/minimum_length_of_string_after_deleting_similar_ends.py
+++ Two Pointers/minimum_length_of_string_after_deleting_similar_ends.py
@@ -16,4 +16,21 @@ class Solution: #Time complexity O(n), Space complexity O(1)
                 r -= 1
 
         return r - l + 1
+
+class MySolution: #Same complexity as above
+    def minimumLength(self, s: str) -> int:
+        l = 0
+        r = len(s) - 1
+
+
+        while l < r and s[l] == s[r]:
+            while l < r and s[l+1] == s[l]:
+                l += 1
+            while r > l and s[r-1] == s[r]:
+                r -= 1
+            l += 1
+            r -= 1
+
+        return 0 if l > r else r - l + 1
+        
         
