818608773c2b5c9afc3654f3edf93333ff4bd8f8
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-06-29 21:03:13 +0100

    Update get_equal_substrings_within_budget.py

--- Sliding Window/get_equal_substrings_within_budget.py
+++ Sliding Window/get_equal_substrings_within_budget.py
@@ -21,4 +21,25 @@ class Solution: #Time complexity O(n), Space complexity O(1)
 
         return res
 
+
+class MySolution: #Time complexity O(n), Space complexity O(1)
+    def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+        res = 0
+        cost = 0
+        l = 0
+        
+        for r in range(len(s)):
+            cost += abs(ord(s[r])-ord(t[r]))
+
+            while cost > maxCost:
+                cost -= abs(ord(s[l])-ord(t[l]))
+                l += 1
+
+            res = max(res, r-l+1)
+
+        return res
+
+
+        
+
         
