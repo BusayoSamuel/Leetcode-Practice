8e096f9c1a608ac06bca088bc919cf8058f32eec
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-06-30 08:36:25 +0100

    Update subtree_of_another_tree.py

--- Trees/subtree_of_another_tree.py
+++ Trees/subtree_of_another_tree.py
@@ -30,3 +30,35 @@ class Solution: #Time complexity O(root * subRoot), Space complexity O(root * su
             return True
 
         return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class MySolution: #Same complexity as above
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+
+        if self.isSameTree(root, subRoot):
+            return True
+
+        if not root:
+            return False
+
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+
+    def isSameTree(self, node1, node2):
+            if not node1 and not node2:
+                return True
+
+            if not node1 or not node2 or node1.val != node2.val:
+                return False
+
+            return self.isSameTree(node1.left, node2.left) and self.isSameTree(node1.right, node2.right)
+
+        
+
+        
