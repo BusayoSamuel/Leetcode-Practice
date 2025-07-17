465c87fb47bf143184e98d6599c750c4cacce561
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-07-17 08:02:36 +0100

    Update length_of_longest_subarray_with_at_most_k_frequency.py

--- Sliding Window/length_of_longest_subarray_with_at_most_k_frequency.py
+++ Sliding Window/length_of_longest_subarray_with_at_most_k_frequency.py
@@ -18,4 +18,26 @@ class MySolution: #Time complexity O(n), Space complexity O(n)
             res = max(res, r - l + 1)
 
         return res
+
+
+class MyOtherSolution: #Same complexity as above
+    def maxSubarrayLength(self, nums: List[int], k: int) -> int:
+        count = defaultdict(int)
+        l = 0
+        res = 0
+
+        for r in range(len(nums)):
+            count[nums[r]] += 1
+
+            while l <= r and count[nums[r]] > k:
+                count[nums[l]] -= 1
+                l += 1
+
+            res = max(res, r-l+1)
+
+
+        return res
+
+
+        
             
