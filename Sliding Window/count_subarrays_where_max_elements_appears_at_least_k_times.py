641281f2d8dbe5a998ad25058c29a461ff51f5e6
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-07-24 08:36:57 +0100

    Update count_subarrays_where_max_elements_appears_at_least_k_times.py

--- Sliding Window/count_subarrays_where_max_elements_appears_at_least_k_times.py
+++ Sliding Window/count_subarrays_where_max_elements_appears_at_least_k_times.py
@@ -38,4 +38,25 @@ class MyEffiecientSolution: #Time complexity O(n), Space complexity O(1)
         return res
 
 
+class ReadableSolution: #Same complexity as above
+    def countSubarrays(self, nums: List[int], k: int) -> int:
+        max_n, max_cnt = max(nums), 0
+        l = 0
+        res = 0
+
+        for r in range(len(nums)):
+            if nums[r] == max_n:
+                max_cnt += 1
+            
+            while max_cnt > k or (l <= r and max_cnt == k and nums[l] != max_n):
+                if nums[l] == max_n:
+                    max_cnt -= 1
+                l += 1
+            
+            if max_cnt == k:
+                res += l + 1
+        
+        return res
+
+
         
