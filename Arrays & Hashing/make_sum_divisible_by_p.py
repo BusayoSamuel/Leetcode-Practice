7cb094ddbb5178fe5eb5f86f6160d774ac268f9f
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-07-25 20:50:45 +0100

    array & hashing qs

---
+++ Arrays & Hashing/make_sum_divisible_by_p.py
@@ -0,0 +1,24 @@
+"""
+https://leetcode.com/problems/make-sum-divisible-by-p/description/
+"""
+
+class Solution: #Time complexity O(n), Space complexity O(n)
+    def minSubarray(self, nums: List[int], p: int) -> int:
+        total = sum(nums)
+        remain = total % p
+        if remain == 0:
+            return 0
+
+        res = len(nums)
+        cur_sum = 0
+        remain_to_idx = {0: -1}
+
+        for i, n in enumerate(nums):
+            cur_sum = (cur_sum + n) % p
+            prefix = (cur_sum - remain + p) % p
+            if prefix in remain_to_idx:
+                length = i - remain_to_idx[prefix]
+                res = min(res, length)
+            remain_to_idx[cur_sum] = i
+
+        return -1 if res == len(nums) else res
