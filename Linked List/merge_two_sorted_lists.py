16243b3ae03f2c3290a5d72dfc415386b517854c
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-07-31 08:04:33 +0100

    Update merge_two_sorted_lists.py

--- Linked List/merge_two_sorted_lists.py
+++ Linked List/merge_two_sorted_lists.py
@@ -23,3 +23,38 @@ class Solution: #O(n) time complexity, #O(1) space complexity
             curr.next = list2
 
         return dummy.next
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class MySolution: #Same complexity as above
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        l1 = list1
+        l2 = list2
+        dummy = ListNode()
+        cur = dummy
+
+        while l1 and l2:
+            if l1.val <= l2.val:
+                temp = l1.next
+                cur.next = l1
+                cur.next.next = None
+                l1 = temp
+            else:
+                temp = l2.next
+                cur.next = l2
+                cur.next.next = None
+                l2 = temp
+
+            cur = cur.next
+
+        if l1:
+            cur.next = l1
+        
+        if l2:
+            cur.next = l2
+
+        return dummy.next
