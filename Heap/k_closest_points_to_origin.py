ec2adfdff7f98d88ead2baf6924be281b70c7eb3
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-08-17 22:11:28 +0100

    Update k_closest_points_to_origin.py

--- Heap/k_closest_points_to_origin.py
+++ Heap/k_closest_points_to_origin.py
@@ -46,4 +46,20 @@ class MyShorterSolution: #Same complexity as above
 
         return res
 
+
+class MyOtherSolution: #Same complexity as above
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        for i, v in enumerate(points):
+            x, y = v
+            points[i] = [(x**2 + y**2), x, y]
+
+        heapq.heapify(points)
+
+        res = []
+        for _ in range(k):
+            dist, x, y = heapq.heappop(points)
+            res.append([x, y])
+
+        return res
+
         
