4504d168ee19c0c186d606abafd2e8920e01d977
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-09-11 21:16:18 +0100

    Update intersection_of_two_linked_list.py

--- Linked List/intersection_of_two_linked_list.py
+++ Linked List/intersection_of_two_linked_list.py
@@ -36,5 +36,31 @@ class Solution2: #Elegant solution, O(n+m) time complexity, #O(1) space complexi
             l1 = l1.next if l1 else headB
             l2 = l2.next if l2 else headA
         return l1 #l1 and l2 will either intersect at a node or None, either way the intersection gets returned
-        
-    
+
+
+class Solution: #O(n+m)  time complexity, O(1) space complexity
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        def getLength(head):
+            length, cur = 0, head
+            while cur:
+                length += 1
+                cur = cur.next
+            return length
+
+        m = getLength(headA)
+        n = getLength(headB)
+        l1, l2 = headA, headB
+
+        if m < n:
+            m, n = n, m
+            l1, l2 = headB, headA
+
+        while m - n:
+            m -= 1
+            l1 = l1.next
+
+        while l1 != l2:
+            l1 = l1.next
+            l2 = l2.next
+
+        return l1
