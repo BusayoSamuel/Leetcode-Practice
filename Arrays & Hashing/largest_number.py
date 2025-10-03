37bcd3f4d530b74d7590dc5ff1e808fab473fa05
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-10-03 08:34:15 +0100

    arrays & hashing qs

---
+++ Arrays & Hashing/largest_number.py
@@ -0,0 +1,16 @@
+"""
+https://leetcode.com/problems/largest-number/description/
+"""
+
+class Solution: #Time complexity O(nlonn) Space complexity O(n)
+    def largestNumber(self, nums: List[int]) -> str:
+        for i, num in enumerate(nums):
+            nums[i] = str(num)
+
+        def compare(n1, n2):
+            if n1 + n2 > n2 + n1:
+                return -1
+            else:
+                return 1
+        nums = sorted(nums, key=cmp_to_key(compare))
+        return str(int("".join(nums)))
