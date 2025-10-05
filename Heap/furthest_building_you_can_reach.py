cdcf2a504f6795a88a1fea6cf752657fed4c0596
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-10-05 09:02:38 +0100

    Update furthest_building_you_can_reach.py

--- Heap/furthest_building_you_can_reach.py
+++ Heap/furthest_building_you_can_reach.py
@@ -24,3 +24,21 @@ class Solution: #Time complexity O(nlogn), Space complexity O(n)
                 bricks += -heapq.heappop(maxheap) #we swap for the highest difference encountered so far
 
         return len(heights) - 1 #this means we can reach the very end
+
+
+class AlternativeSolution: #using a min heap #Same complexity as above
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        minHeap = []
+
+        for i in range(len(heights) - 1):
+            diff = heights[i + 1] - heights[i]
+            if diff <= 0:
+                continue
+
+            heapq.heappush(minHeap, diff)
+            if len(minHeap) > ladders: #we use ladders first and then swap for bricks when all ladders are used
+                bricks -= heapq.heappop(minHeap) #swapping for bricks
+                if bricks < 0: #this means we've used all ladders and all bricks
+                    return i
+
+        return len(heights) - 1
