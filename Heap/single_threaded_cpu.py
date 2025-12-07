7e60a2c05c99738c78431e67f123bca59b18718f
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-12-07 22:52:08 +0000

    Add MySolution class for task scheduling
    Implemented MySolution class to manage task order using a min-heap.

--- Heap/single_threaded_cpu.py
+++ Heap/single_threaded_cpu.py
@@ -28,3 +28,40 @@ class Solution: #Time complexity O(nlogn), Space complexity O(n)
 
 
         return res
+
+
+class MySolution: #Same complexity as above
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        minheap = []
+
+        for i, task in enumerate(tasks):
+            tasks[i].append(i)
+
+        tasks.sort(key=lambda x: [x[0], x[1]], reverse=True)
+        curTime = tasks[-1][0]
+        curTask = tasks[-1]
+        res = [tasks[-1][2]]
+        tasks.pop()
+
+        while tasks or minheap:
+            curTime += curTask[1]
+
+            while tasks and tasks[-1][0] <= curTime:
+                order, time, index = tasks.pop()
+                print([order, time, index ])
+                heapq.heappush(minheap, [time, index, order])
+            
+            if minheap:
+                time, index, order = heapq.heappop(minheap)
+                curTask = [order, time, index]
+            else:
+                curTask = tasks[-1]
+                curTime = tasks[-1][0]
+                tasks.pop()
+
+            res.append(curTask[2])
+
+        return res
+
+
+
