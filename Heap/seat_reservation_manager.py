3952308dfdb03e2f0797c4511629b0e3784eb11c
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2025-12-14 21:19:43 +0000

    heap qs
    Added OptimalSeatManager class for efficient seat reservation.

--- Heap/seat_reservation_manager.py
+++ Heap/seat_reservation_manager.py
@@ -4,7 +4,7 @@ https://leetcode.com/problems/seat-reservation-manager/description/
 
 class SeatManager: #Space complexity O(n)
 
-    def __init__(self, n: int): #Time complexity O(n)
+    def __init__(self, n: int): #Time complexity O(nlogn)
         self.minheap = [i for i in range(1, n + 1)]
         heapq.heapify(self.minheap) #Not necessary because the array above is already sorted
         
@@ -16,6 +16,23 @@ class SeatManager: #Space complexity O(n)
 
     def unreserve(self, seatNumber: int) -> None: #Time complexity O(logn)
         heapq.heappush(self.minheap, seatNumber)
+
+
+class OptimalSeatManager:#Space complexity O(n)
+    def __init__(self, n: int): #Time complexity O(1)
+        self.minHeap = []
+        self.nextSeat = 1
+
+    def reserve(self) -> int: #Time complexity O(logn)
+        if self.minHeap:
+            return heapq.heappop(self.minHeap)
+
+        seat = self.nextSeat
+        self.nextSeat += 1
+        return seat
+
+    def unreserve(self, seatNumber: int) -> None: #Time complexity O(logn)
+        heapq.heappush(self.minHeap, seatNumber)
         
 
 
