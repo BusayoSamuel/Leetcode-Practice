f7a4287644efb20e76a3c97ff9b2ff992055ce0e
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2026-01-19 21:49:41 +0000

    Add MyOtherSolution for zigzag level order traversal
    Added a new solution class 'MyOtherSolution' for zigzag level order traversal. Improved code readability and maintained the same time and space complexity.

--- Trees/binary_tree_zigzag_level_order_traversal.py
+++ Trees/binary_tree_zigzag_level_order_traversal.py
@@ -73,7 +73,46 @@ class CleanerSolution: #Same complexity
             res.append(level)
 
         return res
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class MyOtherSolution: #Same complexity as above
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+
+        q = deque([root] if root else [])
+        left = True
+
+        while q:
+            level = []
+            for i in range(len(q)):
+                node = q.popleft()
+
+                if node.left:
+                    q.append(node.left)
                 
+                if node.right:
+                    q.append(node.right)
+
+                level.append(node.val)
+
+            if left:
+                res.append(level)
+            else:
+                res.append(level[::-1])
+
+            left = not left
+
+
+        return res
+
+        
+                
+
 
 
         
