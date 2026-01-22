025e6c51480d4190ccba732405832af66513613f
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2026-01-22 08:27:36 +0000

    Add reorderList method to reorder linked list
    Implemented a method to reorder a linked list in-place.

--- Linked List/reorder_list.py
+++ Linked List/reorder_list.py
@@ -53,3 +53,46 @@ class Solution:  #Time Complexity: O(n), Space Complexity: O(1)
             tmp = l2
             l2 = l2.next
             tmp.next = l1
+
+class MySolution: #Same complexity as above
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head.next or not head.next.next:
+            return head
+            
+        def reverse(node):
+            prev = None
+            cur = node
+
+            while cur:
+                tmp = cur.next
+                cur.next = prev
+                prev = cur
+                cur = tmp
+
+            return prev
+
+        slow = head
+        fast = head.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        l1 = head
+        l2 = slow.next
+        slow.next = None
+
+        l2 = reverse(l2)
+
+        while l2:
+            tmp1 = l1.next
+            tmp2 = l2.next
+            l1.next = l2
+            l2.next = tmp1
+            l1 = tmp1
+            l2 = tmp2
+
+        return head
