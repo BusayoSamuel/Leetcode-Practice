53e78850d8652fe46bb1c6c40be3e10533f5e7dc
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2026-03-08 19:59:30 +0000

    Implement brute-force twoSum method
    Added a brute-force solution for two-sum problem.

--- Two Pointers/two_sum_ii_input_array_is_sorted.py
+++ Two Pointers/two_sum_ii_input_array_is_sorted.py
@@ -17,4 +17,15 @@ class Solution: #Time complexity O(n), Space complexity O(1)
             else:
                 return [l + 1, r + 1]
 
+
+class Solution: #Time complexity O(n^2), Space complexity O(1)
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for i in range(len(numbers)):
+            for j in range(i+1, len(numbers)):
+                if numbers[i] + numbers[j] > target:
+                    break
+                elif numbers[i] + numbers[j] == target:
+                    return [i + 1, j + 1]
+        
+
         
