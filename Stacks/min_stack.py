78cd052a73eb992be2eba4d875ee3f6b3f0e5206
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2026-03-23 08:46:10 +0000

    Implement MinStack with push, pop, top, and getMin

--- Stacks/min_stack.py
+++ Stacks/min_stack.py
@@ -21,7 +21,24 @@ class MinStack: #O(1) time complexity, #O(n) space complexity
 
     def getMin(self) -> int:
         return self.stack[-1][1]
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = [] #[val, curMin] 
         
+    def push(self, val: int) -> None:        
+        self.stack.append([val, min(val, self.stack[-1][1]) if self.stack else val])
+        
+    def pop(self) -> None:
+        self.stack.pop()
+        
+    def top(self) -> int:
+        return self.stack[-1][0]
+        
+    def getMin(self) -> int:
+        return self.stack[-1][1]
+
 
 
 # Your MinStack object will be instantiated and called as such:
