21a2b1d961d473b27e3d802d3a4d7248118cc9c2
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2026-03-30 12:19:26 +0100

    Implement searchMatrix method in MySolution class
    Added MySolution class with searchMatrix method for searching a target in a 2D matrix using binary search.

--- Binary Search/search_a_2d_matrix.py
+++ Binary Search/search_a_2d_matrix.py
@@ -57,3 +57,34 @@ class Solution: #Same complexity
                 top = center + 1
 
         return False
+
+class MySolution: #Same complexity as above
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        top = 0
+        bottom = len(matrix) - 1
+
+        while top <= bottom:
+            mid = (top + bottom)//2
+
+            if target < matrix[mid][0]:
+                bottom = mid - 1
+            elif target > matrix[mid][-1]:
+                top = mid + 1
+            else:
+                l = 0
+                r = len(matrix[mid]) - 1
+
+                while l <= r:
+                    m = (l + r)//2
+
+                    if target > matrix[mid][m]:
+                        l = m + 1
+                    elif target < matrix[mid][m]:
+                        r = m - 1
+                    else:
+                        return True
+                return False
+        
+        return False
+
+
