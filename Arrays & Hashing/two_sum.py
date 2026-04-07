61005a519dd71c38482bee6b60af7d9146299248
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2026-04-07 09:04:14 +0100

    Refactor two_sum function and add class structure
    Refactor two_sum function for improved readability and maintainability.

--- Arrays & Hashing/two_sum.py
+++ Arrays & Hashing/two_sum.py
@@ -11,3 +11,10 @@ class Solution: #O(n) time complexity, #O(n) space complexity
                 return [hashmap[target-nums[i]], i]
             
             hashmap[nums[i]] = i
+
+class Solution: #O(n^2) time complexity, #O(1) space complexity
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
