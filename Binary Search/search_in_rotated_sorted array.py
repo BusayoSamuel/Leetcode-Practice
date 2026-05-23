7910b04ae553abf618a3e8c7d503e01d8d6774f1
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2026-05-23 09:36:22 +0100

    Implement search method in MySolution class
    Added MySolution class with a search method for finding a target in a rotated sorted array.

--- Binary Search/search_in_rotated_sorted array.py
+++ Binary Search/search_in_rotated_sorted array.py
@@ -85,4 +85,30 @@ class Solution3:  #Same complexity, different structure
                 return m
 
         return -1
+
+
+class MySolution: #Same complexity as above
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0 
+        r = len(nums) - 1
+
+        while l <= r:
+            m = (r + l)//2
+            if nums[m] == target:
+                return m
+
+            if nums[m] < nums[r]:
+                if nums[m] < target <= nums[r]:
+                    l = m + 1
+                else:
+                    r = m - 1
+            else:
+                if nums[l] <= target < nums[m]:
+                    r = m - 1
+                else:
+                    l = m + 1
+
+        return -1
+
+        
         
