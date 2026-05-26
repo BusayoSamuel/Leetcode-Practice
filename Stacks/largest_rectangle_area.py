edebba9779363ba192995aeb5ad49be4881984ed
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2026-05-26 11:05:32 +0100

    stacks qs
    Implement largest rectangle area calculation in histogram.

---
+++ Stacks/largest_rectangle_area.py
@@ -0,0 +1,21 @@
+"""
+https://leetcode.com/problems/largest-rectangle-in-histogram/
+"""
+
+
+class MySolution: #Time complexity O(n), Space complexity O(n)
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        maxArea = 0
+        stack = []  # pair: (index, height)
+
+        for i, h in enumerate(heights):
+            start = i
+            while stack and stack[-1][1] > h:
+                index, height = stack.pop()
+                maxArea = max(maxArea, height * (i - index))
+                start = index
+            stack.append((start, h))
+
+        for i, h in stack:
+            maxArea = max(maxArea, h * (len(heights) - i))
+        return maxArea
