91dc67ae365b1f17ea30574007e6931e6a8f4828
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2026-06-08 18:52:12 +0100

    Implement productExceptSelf method in MySolution class
    Added MySolution class with productExceptSelf method to calculate product of array except self using prefix and postfix arrays.

--- Arrays & Hashing/product_of_array_except_self.py
+++ Arrays & Hashing/product_of_array_except_self.py
@@ -32,3 +32,34 @@ class MySolution: #Time complexity 0(n), Space complexity O(n)
                 res.append(suffix[i+1] * prefix[i-1])
 
         return res
+
+class MySolution: #Same complexity as above
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prefix = [1] * len(nums)
+        postfix = [1] * len(nums)
+
+        curr = 1
+        for i in range(len(nums)):
+            curr = curr * nums[i]
+            prefix[i] = curr
+
+        curr = 1
+        for j in range(len(nums)-1, -1, -1):
+            curr = curr * nums[j]
+            postfix[j] = curr
+
+        res = []
+        curr = 1
+
+        for k in range(len(nums)):
+            pre = prefix[k-1] if k > 0 else 1
+            post = postfix[k+1] if k < len(nums) - 1 else 1
+
+            res.append(pre * post)
+
+        return res
+
+
+        
+
+
