7d7592d78d4f801907ced1fe6505d9026ea2cb22
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2026-06-29 10:47:51 +0100

    Implement alternative solution for permutation check
    Added MyOtherSolution class with an alternative method to check for inclusion of a permutation of s1 in s2.

--- Sliding Window/permutation_in_string.py
+++ Sliding Window/permutation_in_string.py
@@ -70,4 +70,30 @@ class MySolution: #Same complexity as above
                 return True
 
         return False
+
+class MyOtherSolution: #Same complexity as above
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1Count = Counter(s1)
+        s2Count = {}
+
+        l = 0
+
+        for r in range(len(s2)):
+            s2Count[s2[r]] = s2Count.get(s2[r], 0) + 1
+
+            while l <= r and r - l + 1 > len(s1):
+                s2Count[s2[l]] -= 1
+                if s2Count[s2[l]] == 0:
+                    del s2Count[s2[l]]
+                l += 1
+
+
+            if s1Count == s2Count:
+                return True
+
+        return False
+
+
+
+
         
