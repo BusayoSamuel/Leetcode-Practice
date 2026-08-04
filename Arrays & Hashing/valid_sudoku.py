59087895daa21d456a0c5479099338b100db537a
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2026-08-04 09:23:12 +0100

    Implement alternative Sudoku validation solution
    Added MyOtherSolution class with an alternative method to validate a Sudoku board.

--- Arrays & Hashing/valid_sudoku.py
+++ Arrays & Hashing/valid_sudoku.py
@@ -23,3 +23,31 @@ class MySolution: #Time complexity O(1), Space complexity O(1) cause the size of
                         boxes[(i//3), (j//3)].add(board[i][j])
 
         return True
+
+
+class MyOtherSolution: #Same complexity as above
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = [set() for i in range(len(board[0]))]
+        rows = [set() for i in range(len(board))]
+        grid = [ [set() for i in range(3)] for j in range(3)]
+
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if board[r][c] != ".":
+                    if board[r][c] in cols[c]:
+                        return False
+                    else:
+                        cols[c].add(board[r][c])
+                    
+                    if board[r][c] in rows[r]:
+                        return False
+                    else:
+                        rows[r].add(board[r][c])
+
+                    if board[r][c] in grid[r // 3][c // 3]:
+                        return False
+                    else:
+                        grid[r // 3][c // 3].add(board[r][c])
+
+        return True
+        
