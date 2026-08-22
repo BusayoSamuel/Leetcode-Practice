89acc5593ca12da89ada944ac02a3baf403974b4
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2026-08-22 10:41:07 +0100

    Implement recursive tree inversion method
    Refactor invertTree method to use recursion for tree inversion.

--- Trees/invert_binary_tree.py
+++ Trees/invert_binary_tree.py
@@ -36,3 +36,19 @@ class MySolution: #Same complexity as above
 
         dfs(root)
         return root
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class MySolution: #Same complexity as above
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        root.left, root.right  = self.invertTree(root.right), self.invertTree(root.left)
+
+        return root
