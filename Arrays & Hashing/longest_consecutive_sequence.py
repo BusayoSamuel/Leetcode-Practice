3527f37f8f7d71a5d840ed2e5150a8fb9df3f4cf
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2026-09-04 08:29:15 +0100

    Implement AnotherSolution for longest consecutive sequence
    Added AnotherSolution class with an alternative method for finding the longest consecutive sequence.

--- Arrays & Hashing/longest_consecutive_sequence.py
+++ Arrays & Hashing/longest_consecutive_sequence.py
@@ -22,3 +22,23 @@ class Solution: #Time complexity O(n), Space complexity O(n)
             count = 0
 
         return res
+
+
+class AnotherSolution: #Same complexity as above
+    def longestConsecutive(self, nums: List[int]) -> int:
+        hashset = set(nums)
+        res = 0
+
+        for num in nums:
+            if num - 1 not in hashset:
+                count = 1
+                cur = num + 1
+
+                while cur in hashset:
+                    count += 1
+                    cur += 1
+
+                res = max(count, res)
+
+        return res
+        
