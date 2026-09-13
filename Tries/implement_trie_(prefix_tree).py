b37bd36fd84e8dc073e0c6dea3add51751f37fea
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2026-09-13 09:34:40 +0100

    Implement Trie and PrefixTree classes

--- Tries/implement_trie_(prefix_tree).py
+++ Tries/implement_trie_(prefix_tree).py
@@ -44,3 +44,42 @@ class MyTrie: #Time complexity O(n), Space complexity O(n^m) where n is the aver
 # obj.insert(word)
 # param_2 = obj.search(word)
 # param_3 = obj.startsWith(prefix)
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = False
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def insert(self, word: str) -> None:
+            curr = self.root
+            for c in word:
+                if c not in curr.children:
+                    curr.children[c] = TrieNode()
+                curr = curr.children[c]
+            curr.word = True
+
+    def search(self, word: str) -> bool:
+            curr = self.root
+            for c in word:
+                if c not in curr.children:
+                    return False
+                curr = curr.children[c]
+            return curr.word
+        
+
+    def startsWith(self, prefix: str) -> bool:
+            curr = self.root
+            for c in prefix:
+                if c not in curr.children:
+                    return False
+                curr = curr.children[c]
+            return True
+        
+        
