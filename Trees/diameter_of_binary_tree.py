03022d6289be8b51802f4cb6dad10f041b0b4b85
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2026-09-26 11:39:48 +0100

    Implement alternative diameter calculation method
    Added an alternative solution for calculating the diameter of a binary tree using depth-first search.

--- Trees/diameter_of_binary_tree.py
+++ Trees/diameter_of_binary_tree.py
@@ -49,4 +49,33 @@ class MySolution: #Same complexity as above
 
         dfs(root)
         return res
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class MyOtherSolution: #Same complexity as above
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        res = 0
+
+        def dfs(node):
+            nonlocal res
+
+            if not node:
+                return -1
+
+            left = 1 + dfs(node.left)
+            right = 1 + dfs(node.right)
+
+            res = max(res, left + right)
+            return max(left, right)
+
+        dfs(root)
+
+        return res
+
+        
         
